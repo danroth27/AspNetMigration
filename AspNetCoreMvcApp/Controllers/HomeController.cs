010d8b0b140a3ebe926e93f2eb1086ef6da1ea9d
Daniel Roth <daroth@microsoft.com>
2022-05-30 21:44:21 -0700

    Migrate contact page

--- AspNetCoreMvcApp/Controllers/HomeController.cs
+++ AspNetCoreMvcApp/Controllers/HomeController.cs
@@ -20,11 +20,11 @@ namespace AspNetMvcApp.Controllers
             return View();
         }
 
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-        //    return View();
-        //}
+            return View();
+        }
     }
 }
